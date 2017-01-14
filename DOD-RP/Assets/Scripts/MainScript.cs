9bfeaadcd7a9a5e64f0cb53800f6f528ecfbc52a
Tubbles <jae91m@gmail.com>
2017-01-14 20:52:09 +0100

    Trying to fix button

---
+++ DOD-RP/Assets/Scripts/MainScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape)) 
+		{
+			quitGame();
+		}
+	}
+
+	public void quitGame()
+	{
+		#if UNITY_EDITOR || UNITY_EDITOR_64
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+}
